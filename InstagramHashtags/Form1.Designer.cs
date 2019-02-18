5e7f4879aacfdf98e99316f1dec603157f1c179c
mrrobot <kooldude5347@ahoo.com>
2019-02-17 20:21:55 -0500

    adding files

---
+++ InstagramHashtags/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace InstagramHashtags
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(12, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(242, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Scrape";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(132, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(15, 100);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(239, 209);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(12, 341);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(242, 24);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(93, 318);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Count:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(137, 318);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "0";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(206, 312);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(48, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Copy";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(132, 36);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(122, 20);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(9, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Niche Characteristic 1:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(9, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Niche Characteristic 2:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(15, 397);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(96, 20);
+            this.textBox4.TabIndex = 21;
+            this.textBox4.Text = "Phone Number";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(96, 381);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Send TxT";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(206, 394);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(48, 23);
+            this.button4.TabIndex = 23;
+            this.button4.Text = "Send";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "AT&T",
+            "Verizon"});
+            this.comboBox1.Location = new System.Drawing.Point(117, 396);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(83, 21);
+            this.comboBox1.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(74)))), ((int)(((byte)(74)))));
+            this.ClientSize = new System.Drawing.Size(276, 430);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Instagram Hashtag Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ InstagramHashtags/Form1.cs
@@ -0,0 +1,429 @@
+﻿using Microsoft.Win32;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InstagramHashtags
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ArrayList hashtags = new ArrayList();
+        ArrayList hashtagsUsed = new ArrayList();
+        ArrayList hashtagPostCount = new ArrayList();
+        int foundHashTagCount = 0;
+        public string memberName;
+        public int rank;
+        public int userID;
+        public string version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        ChromeDriver driver;
+        string profileIconXPath = "//*[@id=\"react-root\"]/section/nav/div[2]/div/div/div[3]/div/div[3]/a";
+
+
+        async private void button1_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                button2_Click(null,null); //clear
+                //texbox 1 filled out only
+                if (textBox1.Text.Length > 0 && textBox3.Text.Length == 0)
+                {
+                    await scrape(textBox1.Text);
+                }//texboxs both filled out
+                else if (textBox1.Text.Length > 0 && textBox3.Text.Length > 0)
+                {
+                    await scrape(textBox1.Text);
+                    await scrape(textBox3.Text);
+                }//textbox 3 only filled out
+                else if (textBox1.Text.Length == 0 && textBox3.Text.Length > 0)
+                {
+                    await scrape(textBox3.Text);
+                }//no textboxs filled
+                else if (textBox1.Text.Length == 0 && textBox3.Text.Length == 0)
+                {
+                    MessageBox.Show("Please input your niche characteristics");
+                    return;
+                }
+
+
+
+                button2.BackColor = button1.BackColor;
+                button2.Text = "Dont Like Your Hashtags? Clear";
+                button2.Enabled = true;
+                button1.Enabled = true;
+                label2.Text = (textBox2.Lines.Length - 1).ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(hashtags.Count.ToString() + ":" + ex.ToString());
+                button2.BackColor = button1.BackColor;
+                button2.Text = "Dont Like Your Hashtags? Clear";
+                button2.Enabled = true;
+                button1.Enabled = true;
+                label2.Text = (textBox2.Lines.Length - 1).ToString();
+            }
+        }
+
+        async Task waitForElement(string xPath)
+        {
+            try
+            {
+                await Task.Delay(500);
+                int trys = 0;
+                int count = 0;
+                do
+                {
+                    if (xPath.Contains("/"))
+                    {
+                        var links = driver.FindElementsByXPath(xPath);
+                        count = links.Count;
+                    }
+                    else
+                    {
+                        var links = driver.FindElementsByClassName(xPath);
+                        count = links.Count;
+                    }
+
+                    trys++;
+                    await Task.Delay(1000);
+                } while (count == 0 && trys < 10);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        async private Task scrape(string hashT)
+        {
+            try
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button2.Text = "Gathering Best Hashtags..";
+                button2.BackColor = Color.DarkSeaGreen;
+                //strip the text
+                ArrayList audience = new ArrayList();
+                string temp = "";
+                string x = hashT;
+                if (x.Contains("#") || x.Contains(" "))
+                {
+                    MessageBox.Show("Dont use #");
+                    return;
+                }
+                for (int i = 0; i < x.Length; i++)
+                {
+                    string letter = x.Substring(i, 1);
+                    if (letter.Equals(""))
+                    {
+                        continue;
+                    }
+                    if (!letter.Equals(","))
+                    {
+                        temp += letter;
+                    }
+                    else
+                    {
+                        audience.Add(temp);
+                        temp = "";
+                    }
+                    if (i == x.Length - 1)
+                    {
+                        audience.Add(temp);
+                    }
+                }
+                //done stripping text
+                Random rnd = new Random();
+                ArrayList hashtag = new ArrayList();
+                foreach (string searchItem in audience)
+                {
+                    await search("https://www.instagram.com/bob", profileIconXPath);
+                    driver.FindElementByClassName("TqC_a").Click(); //click on search bar to start typing
+                    await Task.Delay(300);
+                    IWebElement searchBar = driver.FindElementByClassName("XTCLo");
+                    searchBar.Clear();
+                    searchBar.SendKeys("#" + searchItem); //send niche to searchbar
+                    await waitForElement("_2_M76");
+                    var tagNames = driver.FindElementsByClassName("Ap253"); //each tag that appears after typing in
+                    if (tagNames.Count == 0)
+                    {
+                        textBox2.Text += "Bad tag to find hashtag: " + searchItem + searchItem + Environment.NewLine;
+                        continue;
+                    }
+                    string randomHash = tagNames[rnd.Next(0, tagNames.Count / 2)].Text;
+                    for (int i = 0; i < 15; i++)
+                    {
+                        randomHash = tagNames[rnd.Next(0, tagNames.Count)].Text;
+                        string xx = randomHash.Substring(1);
+                        if(!hashtag.Contains(xx)){
+                            hashtag.Add(xx);
+                        }
+                        else
+                        {
+                            i--;
+                        }
+                        await Task.Delay(500);
+                    }
+                    
+                }
+                textBox4.Text = "";
+                foreach (string item in hashtag)
+                {
+                    textBox2.Text += "#"+item + Environment.NewLine;
+                    await Task.Delay(500);
+                    label2.Text = textBox2.Lines.Count().ToString();
+                }
+
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button2.Text = "Clear";
+                button2.BackColor = Color.Silver;
+
+            }
+            catch (Exception ex)
+            {
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button2.Text = "Clear";
+                button2.BackColor = Color.Silver;
+                return;
+            }
+        }
+       
+        
+        public static void doSelectionSort(ArrayList array, ArrayList array2)
+        {
+            string tmp = "";
+            int min_key;
+
+            for (int j = 0; j < array.Count - 1; j++)
+            {
+                min_key = j;
+
+                for (int k = j + 1; k < array.Count; k++)
+                {
+                    if (Convert.ToInt32(array[k].ToString()) > Convert.ToInt32(array[min_key].ToString()))
+                    {
+                        min_key = k;
+                    }
+                }
+
+                tmp = array[min_key].ToString();
+                array[min_key] = array[j];
+                array[j] = tmp;
+
+                tmp = array2[min_key].ToString();
+                array2[min_key] = array2[j];
+                array2[j] = tmp;
+
+            }
+        }
+
+        private void addHashtagsToList()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                if (hashtags.Count <= 0)
+                {
+                    break;
+                }
+                Random rnd = new Random();
+                int randNum = rnd.Next(hashtags.Count);
+
+                if (Regex.IsMatch(hashtags[randNum].ToString().Substring(1), @"^[a-zA-Z0-9_]+$"))
+                {
+                    textBox2.Text += hashtags[randNum];
+                    hashtagsUsed.Add(hashtags[randNum]);
+                    hashtags.RemoveAt(randNum);
+                    hashtagPostCount.RemoveAt(randNum);
+                    textBox2.Text += Environment.NewLine;
+                }
+                else
+                {
+                    hashtagsUsed.Add(hashtags[randNum]);
+                    hashtags.RemoveAt(randNum);
+                    hashtagPostCount.RemoveAt(randNum);
+                }
+            }
+        }
+
+       
+        private int NthIndexOf(string target, string value, int n)
+        {
+            Match m = Regex.Match(target, "((" + Regex.Escape(value) + ").*?){" + n + "}");
+
+            if (m.Success)
+                return m.Groups[2].Captures[n - 1].Index;
+            else
+                return -1;
+        }
+
+        async public Task search(string url, string xPathElementToWaitFor)
+        {
+            int trys = 0;
+            driver.Navigate().GoToUrl(url);
+            if (xPathElementToWaitFor.Length > 0)
+            {
+
+                await waitForElement(xPathElementToWaitFor);
+            }
+            else
+            {
+                if (trys >= 10)
+                {
+                    return;
+                }
+                trys++;
+                await Task.Delay(1000);
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\AppEvents\\Schemes\\Apps\\Explorer\\Navigating\\.Current", "", "NULL");
+            textBox1.Focus();
+           
+            
+            ChromeDriverService driverService = ChromeDriverService.CreateDefaultService();
+            driverService.HideCommandPromptWindow = true;
+            ChromeOptions options = new ChromeOptions();
+            //options.AddArgument("--headless");
+            driver = new ChromeDriver(driverService, options);
+        }
+
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(textBox2.Text.Length == 0){
+                return;
+            }
+            string textforClipboard = textBox2.Text;
+            Clipboard.Clear();
+            Clipboard.SetText(textforClipboard);
+            MessageBox.Show("Hashtags Copied To Clipboard");
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(!button1.Enabled){
+                return;
+            }
+            if (e.KeyCode == System.Windows.Forms.Keys.Enter)
+            {
+                button1_Click(null, null);
+                // these last two lines will stop the beep sound
+                e.SuppressKeyPress = true;
+                e.Handled = true;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://ogusers.com/MrRobot");
+        }
+
+
+       
+       
+        private string fixMessage(string txt)
+        {
+            string x = "";
+            for (int i = 0; i < txt.Length;i++ )
+            {
+                string letter = txt.Substring(i,1);
+                if(letter.Equals("#")){
+                    x += " #";
+                }
+                else
+                {
+                    x += letter;
+                }
+            }
+            return x;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string message = fixMessage(textBox2.Text);
+            sendMessage(textBox4.Text,message);
+        }
+
+        public String getCarrier(int x)
+        {
+            switch (x)
+            {
+                case 0:
+                    return "@txt.att.net"; //ATT
+                case 1:
+                    return "@vtext.com"; //VERIZON
+                case 2:
+                    return "@messaging.sprintpcs.com"; //SPRINT
+                case 3:
+                    return "@tmomail.net"; //T-MOBILE  
+                default:
+                    return "ERROR";
+            }
+        }
+
+        public void sendMessage(String number, String message)
+        {
+            string carrier = getCarrier(comboBox1.SelectedIndex);
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress("ddos7003@gmail.com");
+                mail.Sender = new MailAddress("ddos7003@gmail.com");
+                mail.To.Add(number + carrier);
+                mail.IsBodyHtml = true;
+                mail.Subject = "Hashtags for: "+textBox1.Text+","+textBox3.Text;
+                mail.Body = message;
+
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                smtp.UseDefaultCredentials = false;
+
+                smtp.Credentials = new System.Net.NetworkCredential("ddos7003@gmail.com", "chris5347");
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.EnableSsl = true;
+
+                smtp.Send(mail);
+                MessageBox.Show("Text Sent: "+number+carrier);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(("ERROR: " + e.ToString()));
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            hashtags.Clear();
+            hashtagsUsed.Clear();
+            hashtagPostCount.Clear();
+            label2.Text = "0";
+        }
+        
+
+
+
+
+    }
+}
